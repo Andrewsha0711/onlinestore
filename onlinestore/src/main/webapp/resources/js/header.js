d6e85d167a38e8dc6617261181b5a90a7b185b4b
Andrew <andrewdecker07@gmail.com>
2022-05-08 18:10:55 +0300

    sign-in and sign-up pop-up

--- onlinestore/src/main/webapp/resources/js/header.js
+++ onlinestore/src/main/webapp/resources/js/header.js
@@ -1,20 +1,43 @@
 /*
 Выплывающие меню
  */
+ function createHeader(){
+   const nav = document.getElementById('nav');
+   fetch('./header.html')
+   .then(res=>{
+     return res.text();
+   })
+   .then(data=>{
+     nav.innerHTML=data;
+   })
+ }
 
-function profileMenuClick(){
-  const toggleMenu = document.querySelector('.profile-pop-up-menu .menu');
+function profileIconClick(){
   if(1==0){
+    const toggleMenu = document.querySelector('.profile-pop-up-menu .menu');
     toggleMenu.classList.toggle('active')
   }
   else{
-    var url = window.location.href;
-    console.log(url);
-      window.location = url;
+    const toggleMenu = document.querySelector('.autorization-pop-up');
+    toggleMenu.classList.toggle('active')
   }
 }
 
 function categoriesMenuToggle(){
   const toggleMenu = document.querySelector('.pop-up-categories-menu .menu');
   toggleMenu.classList.toggle('active')
+}
+
+function switchToRegistrationForm(){
+  const regForm = document.querySelector('.registration-form');
+  const loginForm = document.querySelector('.login-form');
+  regForm.classList.add('active');
+  loginForm.classList.add('hidden');
+}
+
+function switchToLoginForm(){
+  const regForm = document.querySelector('.registration-form');
+  const loginForm = document.querySelector('.login-form');
+  regForm.classList.remove('active');
+  loginForm.classList.remove('hidden');
 }
